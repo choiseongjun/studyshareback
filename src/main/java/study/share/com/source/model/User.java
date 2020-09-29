29e89d83b7e46229f1d43510a077db701d67bb21
choiseognjun <csj2702@naver.com>
2020-09-29 13:01:37 +0900

    유저-유저프로필이미지 엔티티 구축

--- src/main/java/study/share/com/source/model/User.java
+++ src/main/java/study/share/com/source/model/User.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,9 +11,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
@@ -68,7 +68,8 @@ public class User{
     @Size(min=6, max = 100)
     @JsonIgnore
     private String password;
-
+    @Lob
+	private String introduce;
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles", 
     	joinColumns = @JoinColumn(name = "user_id"), 
@@ -78,6 +79,10 @@ public class User{
 	private List<FeedLike> feedlike;
     @OneToMany(fetch = FetchType.LAZY,orphanRemoval=true,mappedBy = "user")
 	private List<TodoList> todolist;
+    
+    @OneToOne(mappedBy="user", orphanRemoval = true)
+	private UserProfileImage userProfileImage;
+    
     public User(String userid,String nickname, String email,String sex,String password) {
         this.userid = userid;
         this.nickname = nickname;
---
+++ src/main/java/study/share/com/source/model/UserProfileImage.java
@@ -0,0 +1,36 @@
+package study.share.com.source.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "userprofileimage")
+@Getter
+@Setter
+public class UserProfileImage {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@JsonIgnore
+	private String filename;
+	@JsonIgnore
+	private String realname;
+	@JsonIgnore
+	private long filesize;
+
+	private String src;
+	@JsonIgnore
+	private String ImageExtension;
+	
+	@OneToOne
+	private User user;
+}
