6c4889a158e3e44b3162d56f6898771a0a222a88
HwaGyeong <dlghkrud18@naver.com>
2021-04-27 17:37:06 +0900

    사용자 로깅 기능 구현

--- src/main/java/study/share/com/source/repository/UserHistoryRepository.java
+++ src/main/java/study/share/com/source/repository/UserHistoryRepository.java
@@ -1,8 +1,10 @@
 package study.share.com.source.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import study.share.com.source.model.UserHistory;
 
+@Repository
 public interface UserHistoryRepository extends JpaRepository<UserHistory, Long> {
 
 
